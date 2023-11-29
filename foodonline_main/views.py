c7c2dfbeb9db08ce78644c0d44d3c3949ca2d4a9
cevher özden <efeozden@gmail.com>
2023-11-29 10:41:27 +0300

    database configured

--- foodonline_main/views.py
+++ foodonline_main/views.py
@@ -1,7 +1,8 @@
 
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse("<h3>Home Page</h3>")
+    return render(request, 'home.html')
